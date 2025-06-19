771db9c7ba827f04d53f093a7a74036a89e11584
WilliamDjo <william.djong@yahoo.com>
2025-06-19 22:22:54 +1000

    Added code to sum to n --not tested

---
+++ sum_to_n.asm
@@ -0,0 +1,33 @@
+section .text
+global sum_to_n
+
+sum_to_n:
+	push rbp
+	mov rbp, rsp
+	
+	; if n <= 0, return 0
+	cmp rdi, 0
+	jle return_zero
+
+	mov rax, 0 	; sum
+	mov rcx, 1	; counter
+
+loop_start:
+	cmp rcx, edi
+	jg loop_end	; if counter > n, exit loop
+	
+	add rax, rcx	; sum = sum + counter
+	
+	inc rcx		; counter++
+	
+	jmp loop_start	; go back to start (loop) 
+
+loop_end:
+	jmp done
+
+return_zero:
+	mov eax, 0
+
+done:
+	pop rbp	
+	ret
